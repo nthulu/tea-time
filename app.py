6bc3f87bbcb187840f123d1eac6ec452d290ae2b
nthulu <shaohulu@hotmail.com>
2026-08-29 21:08:36 +0800

    修改版本显示bug 1.0.1

--- app.py
+++ app.py
@@ -68,7 +68,7 @@ if 'tea_time' not in st.session_state:
 if 'is_running' not in st.session_state:
     st.session_state.is_running = False # 当前是否正在倒计时
 if 'app_version' not in st.session_state:
-    st.session_state.app_version = "v1.0.0"  # 版本号
+    st.session_state.app_version = "v1.0.1"  # 版本号
 
 # 创建按钮逻辑
 col1, col2 = st.columns(2) # 把按钮分成两列
@@ -127,4 +127,5 @@ elif st.session_state.tea_time <= 0 and st.session_state.is_running:
     st.session_state.current_step += 1  # 泡数加1
 
     st.info(f"💡 提示：你已经泡了 {st.session_state.current_step - 1} 次，下一泡建议冲泡 {tea_options[selected_tea][st.session_state.current_step - 1] if st.session_state.current_step <= len(tea_options[selected_tea]) else 'N/A'} 秒。" )
-    st.caption(f"🚀 当前应用版本：{st.session_state.app_version}")
+
+st.caption(f"🚀 当前应用版本：{st.session_state.app_version}")
